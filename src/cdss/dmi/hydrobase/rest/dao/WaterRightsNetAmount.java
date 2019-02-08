1cb848358a853477f317c8b4e56aa6f97a5930e1
Steve Malers <steve.malers@openwaterfoundation.org>
2019-02-08 03:47:16 -0700

    Update for OpenCDSS release, issue #23
    All code files have been updated with copyright/license notice.
    Update the README file for GPL 3 license.

--- src/cdss/dmi/hydrobase/rest/dao/WaterRightsNetAmount.java
+++ src/cdss/dmi/hydrobase/rest/dao/WaterRightsNetAmount.java
@@ -1,3 +1,26 @@
+// WaterRightsNetAmount - data object for water right, net amount
+
+/* NoticeStart
+
+CDSS HydroBase REST Web Services Java Library
+CDSS HydroBase REST Web Services Java Library is a part of Colorado's Decision Support Systems (CDSS)
+Copyright (C) 2018-2019 Colorado Department of Natural Resources
+
+CDSS HydroBase REST Web Services Java Library is free software:  you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CDSS HydroBase REST Web Services Java Library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CDSS HydroBase REST Web Services Java Library.  If not, see <https://www.gnu.org/licenses/>.
+
+NoticeEnd */
+
 package cdss.dmi.hydrobase.rest.dao;
 
 import java.time.LocalDateTime;
